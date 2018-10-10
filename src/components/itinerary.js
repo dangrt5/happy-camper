c2c285182fc789a2606f5def02f507903d3963e5
Rachel Pan <rpan06@gmail.com>
2018-10-10 15:56:35 -0700

    more changes

--- src/components/itinerary.js
+++ src/components/itinerary.js
@@ -3,6 +3,13 @@ import "../assets/css/itinerary.css"
 import Header from "./header";
 import ItineraryCard from './itinerary_card'
 
+const dummyData = [{
+  name: "Park Name",
+  address1: "Angeles Crest Hwy,",
+  address2: "Wrightwood, CA 92397",
+  image: "https://thedyrt.imgix.net/photo/52799/photo/alabama-brushy-lake-campground_21af9c56af3dd46ea2516cc2f8b18c5d.jpg"
+}]
+
 export default class Itinerary extends Component {
   constructor(props) {
     super(props);
@@ -18,11 +25,14 @@ export default class Itinerary extends Component {
   }
     render(){
       const {path} = this.state;
+      const {itineraryList} = dummyData.map((item,index)=>{
+          <ItineraryCard key={item} data={item}/>
+      })
         return (
           <div>
             <Header path={path}/>
             <div className="container">
-              <ItineraryCard/>
+              {itineraryList}
             </div>
           </div>
         )
