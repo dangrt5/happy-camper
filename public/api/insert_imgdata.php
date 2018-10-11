1eacef3e20cc90b65a609efc68e1b1b0bb7dde38
citation0097 <32308475+citation0097@users.noreply.github.com>
2018-10-11 14:09:07 -0700

    insert merge 3

--- public/api/insert_imgdata.php
+++ public/api/insert_imgdata.php
@@ -4,40 +4,34 @@ require_once("mysql_connect.php");
 
 $parkHandler = curl_init();
 curl_setopt($parkHandler, CURLOPT_URL, "https://thedyrt.com/api/v2/campgrounds?filter%5Bsearch%5D%5Bregion%5D=CA&include=administrative-area%2Coperator%2Crecent-reviewers&modelPath=controller.model.featuredCampgrounds&page%5Bnumber%5D=1&page%5Bsize%5D=200");
-curl_setopt($parkHandler, CURLOPT_FOLLOWLOCATION, true);
-// curl_setopt($parkHandler, CURLOPT_SSL_VERIFYPEER, false);
-// curl_setopt($parkHandler, CURLOPT_RETURNTRANSFER, 1);
-// $importPark = curl_exec($parkHandler);
-$importPark = file_get_contents('https://thedyrt.com/api/v2/campgrounds?filter%5Bsearch%5D%5Bregion%5D=CA&include=administrative-area%2Coperator%2Crecent-reviewers&modelPath=controller.model.featuredCampgrounds&page%5Bnumber%5D=1&page%5Bsize%5D=200');
+curl_setopt($parkHandler, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($parkHandler, CURLOPT_RETURNTRANSFER, 1);
+// curl_setopt($parkHandler, CURLOPT_FOLLOWLOCATION, true);
+// $importPark = file_get_contents('https://thedyrt.com/api/v2/campgrounds?filter%5Bsearch%5D%5Bregion%5D=CA&include=administrative-area%2Coperator%2Crecent-reviewers&modelPath=controller.model.featuredCampgrounds&page%5Bnumber%5D=1&page%5Bsize%5D=200');
+$importPark = curl_exec($parkHandler);
 $parkList = json_decode($importPark, - true);
 curl_close($parkHandler);
 
-$output =[
-    'success'=> false,
-    'error'  => []
-];
-
-// print_r($parkList);
 ini_set("max_execution_time", 0);
 
 foreach($parkList["data"] as $key ){
  
     $parkID = $key['id'];
-    
     $selectQuery = "SELECT `id` FROM `park_info` WHERE `park_id` = $parkID";
     $result = mysqli_query($conn, $selectQuery);
     $keyID = mysqli_fetch_assoc($result);
 
     $photoHandler = curl_init();
     curl_setopt($photoHandler, CURLOPT_URL, "https://thedyrt.com/api/v2/campgrounds/{$parkID}/photos");
-    curl_setopt($photoHandler, CURLOPT_FOLLOWLOCATION, true);
-    // curl_setopt($photoHandler, CURLOPT_SSL_VERIFYPEER, false);
-    // curl_setopt($photoHandler, CURLOPT_RETURNTRANSFER, 1);
-    // $importPhoto = curl_exec($photoHandler);
-    $importPhoto = file_get_contents("https://thedyrt.com/api/v2/campgrounds/{$parkID}/photos");
+    curl_setopt($photoHandler, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($photoHandler, CURLOPT_RETURNTRANSFER, 1);
+
+    // curl_setopt($photoHandler, CURLOPT_FOLLOWLOCATION, true);
+    // $importPhoto = file_get_contents("https://thedyrt.com/api/v2/campgrounds/{$parkID}/photos");
+    $importPhoto = curl_exec($photoHandler);
     $photoList = json_decode($importPhoto, - true);
     curl_close($photoHandler);
-    // print_r($photoList);
+
     $count = 0;
     
     foreach($photoList["data"] as $item ){
@@ -53,11 +47,9 @@ foreach($parkList["data"] as $key ){
         // print_r($insertQuery);
         $insertResult = mysqli_query($conn, $insertQuery); 
         $insertQuery = '';    
-
         $count++;
     }
-    
-    print('done');
+   
 }   
-
+print('done');
 ?>
