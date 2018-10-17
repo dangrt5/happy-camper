0f0c675206c7f9573f062cb5f7ec44c240794696
krow <rpan06@gmail.com>
2018-10-17 15:05:13 -0700

    removed features for offline status

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -6,12 +6,17 @@ class HamburgerMenu extends Component {
   render() {
     return (
       <div className="sidebar">
-        <div className="pages">
-          <Link to="/"><h1>home</h1></Link>
-          <Link to="/itinerary"><h1>itinerary</h1></Link>
-          <h1>survival guide</h1>
-          <Link to="/about-us"><h1>about us</h1></Link>
-        </div>
+            {window.navigator.onLine ?
+              <div className="pages">
+                <Link to="/"><h1>home</h1></Link>
+                <Link to="/itinerary"><h1>itinerary</h1></Link>
+                <Link to="/about-us"><h1>about us</h1></Link>
+              </div>
+              :
+              <div className="pages">
+                <Link to="/itinerary"><h1>itinerary</h1></Link>
+              </div>
+            }
       </div>
     )
   }
