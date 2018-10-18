8b7c87b37d72c761b5209211832cd5e621ce04c4
krow <rpan06@gmail.com>
2018-10-18 16:16:45 -0700

    added animation and wallpaper to landing page

--- src/components/landing_page.js
+++ src/components/landing_page.js
@@ -3,22 +3,19 @@ import LocationSearchInput from './input_search'
 import '../assets/css/landingPage.css';
 import logo from "../assets/images/happy-camper-logo1-white.png";
 
-import background2 from "../assets/images/backgrounds/wallpaper.jpg"
-
 export default class LandingPage extends Component {
-    componentDidMount(){
-        document.body.style.backgroundImage = `url(${background2})`;
-    }
-    componentWillUnmount(){
-        document.body.style.backgroundImage = '';
-    }
     render(){
         return (
-            <div className="landing-page-container">
-                <img className="logo" src={logo}/>
-                <form>
-                    <LocationSearchInput class="landing-page-input" push={this.props.history.push}/>
-                </form>
+            <div>
+                <div className="landing-page-background1"></div>
+                <div className="landing-page-background2"></div>
+                <div className="landing-page-background3"></div>
+                <div className="landing-page-container">
+                    <img className="logo" src={logo}/>
+                    <form>
+                        <LocationSearchInput class="landing-page-input" push={this.props.history.push}/>
+                    </form>
+                </div>
             </div>
         )
     }
