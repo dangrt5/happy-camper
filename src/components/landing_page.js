7251a4c02d14886aa440600a2bedd4e2da9f0e57
krow <rpan06@gmail.com>
2018-10-18 15:46:40 -0700

    added background to landing page

--- src/components/landing_page.js
+++ src/components/landing_page.js
@@ -1,9 +1,17 @@
 import React, {Component} from 'react';
 import LocationSearchInput from './input_search'
 import '../assets/css/landingPage.css';
-import logo from "../assets/images/happy-camper-logo1.png";
+import logo from "../assets/images/happy-camper-logo1-white.png";
+
+import background2 from "../assets/images/backgrounds/wallpaper.jpg"
 
 export default class LandingPage extends Component {
+    componentDidMount(){
+        document.body.style.backgroundImage = `url(${background2})`;
+    }
+    componentWillUnmount(){
+        document.body.style.backgroundImage = '';
+    }
     render(){
         return (
             <div className="landing-page-container">
