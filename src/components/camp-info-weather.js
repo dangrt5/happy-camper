f3f06f618b58ce281b7629b2e559bdfe446d50e3
David Ahn <38971950+d-ahn10@users.noreply.github.com>
2018-10-18 17:25:45 -0700

    Revert "Clean code"

--- src/components/camp-info-weather.js
+++ src/components/camp-info-weather.js
@@ -9,19 +9,11 @@ export default class Weather extends React.Component {
         weather: [],
     }
     componentDidMount(){
-        if(!window.navigator.onLine){
-            const itinerary = JSON.parse(localStorage.getItem('itinerary'))
-            let item = itinerary.filter(item => item.parkinfo[0].lat === this.props.lat && item.parkinfo[0].lng === this.props.lng)
-            this.setState({
-                weather: item.weather
-            })
-        } else {
-            this.getWeatherData();
-        }
+        this.getWeatherData();
     }
     async getWeatherData(){
         const {lat, lng} = this.props
-        const resp = await axios.get(`https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lng}&units=imperial&cnt=15&appid=${config.WEATHER_KEY}`)
+        const resp = await axios.get(`http://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lng}&units=imperial&cnt=15&appid=${config.WEATHER_KEY}`)
         this.setState({
             weather: resp.data.list
         })
--- src/components/landing_page.js
+++ src/components/landing_page.js
@@ -1,21 +1,16 @@
 import React, {Component} from 'react';
 import LocationSearchInput from './input_search'
 import '../assets/css/landingPage.css';
-import logo from "../assets/images/happy-camper-logo1-white.png";
+import logo from "../assets/images/happy-camper-logo1.png";
 
 export default class LandingPage extends Component {
     render(){
         return (
-            <div>
-                <div className="landing-page-background1"></div>
-                <div className="landing-page-background2"></div>
-                <div className="landing-page-background3"></div>
-                <div className="landing-page-container">
-                    <img className="logo" src={logo}/>
-                    <form>
-                        <LocationSearchInput class="landing-page-input" push={this.props.history.push}/>
-                    </form>
-                </div>
+            <div className="landing-page-container">
+                <img className="logo" src={logo}/>
+                <form>
+                    <LocationSearchInput class="landing-page-input" push={this.props.history.push}/>
+                </form>
             </div>
         )
     }
